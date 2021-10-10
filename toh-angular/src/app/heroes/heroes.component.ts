4aa1660c46e32b9aa542430310ac743addfb3c4b
pablongo <paugallardocordon@gmail.com>
2021-10-10 23:49:57 +0200

    Crete message log of clicked heroes

--- toh-angular/src/app/heroes/heroes.component.ts
+++ toh-angular/src/app/heroes/heroes.component.ts
@@ -1,32 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
-  constructor(private heroService: HeroService) { }
-  
-  heroes: Hero[] = [];
 
   selectedHero?: Hero;
 
-  onSelect(hero:Hero): void {
-    this.selectedHero = hero;
-  }
+  heroes: Hero[] = [];
 
-  getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes)
-  }
-  
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
